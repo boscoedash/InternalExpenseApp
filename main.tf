ef74e42aeb3d21a299ed1f511a21bdb7e27fd3f0
bhlokana <bhlokana@microsoft.com>
2021-02-18 11:44:16 -0600

    update paths

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "ResourceGroup" {
-  source    = "../Modules/ResourceGroup"
+  source    = "./InfastructureAsCode/Modules/ResourceGroup"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
 }

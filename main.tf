9e34cd9bb03f6c643d801cf44d6da6270596d8e4
bhlokana <bhlokana@microsoft.com>
2021-02-18 11:46:35 -0600

    path typo

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "ResourceGroup" {
-  source    = "./InfrastructureAsCode/Modules/ResourceGroup"
+  source    = "./InfrastructureAsCode/Modules/Terraform/ResourceGroup"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
 }

916d05f93481cfa5084cb0f99fea7fea7ae6b291
bhlokana <bhlokana@microsoft.com>
2021-02-18 17:26:04 -0600

    add vnet

--- main.tf
+++ main.tf
@@ -16,3 +16,11 @@ module "ResourceGroup" {
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
 }
+
+module "VirtualNetwork" {
+  source                        = "./InfrastructureAsCode/Modules/Terraform/VirtualNetwork"
+  virtual_network_name          = var.virtual_network_name
+  resource_group_name           = var.resource_group_name
+  resource_group_location       = var.resource_group_location
+  virtual_network_address_space = var.virtual_network_address_space
+}

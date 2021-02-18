ca591d42dd0b76be7c875fc67e6b424b1c95fefd
bhlokana <bhlokana@microsoft.com>
2021-02-18 11:45:18 -0600

    typo

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 module "ResourceGroup" {
-  source    = "./InfastructureAsCode/Modules/ResourceGroup"
+  source    = "./InfrastructureAsCode/Modules/ResourceGroup"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
 }

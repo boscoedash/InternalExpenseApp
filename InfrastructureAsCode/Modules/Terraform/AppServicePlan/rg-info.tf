34ab8bd205598d72a107abe798f1daf04de812a3
bhlokana <bhlokana@microsoft.com>
2021-02-19 14:18:40 -0600

    remove rg info

--- InfrastructureAsCode/Modules/Terraform/AppServicePlan/rg-info.tf
+++
@@ -1,9 +0,0 @@
-
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
-
-locals {
-  tags     = (var.tags == null ? data.azurerm_resource_group.rg.tags : var.tags)
-  location = data.azurerm_resource_group.rg.location
-}

b81f9e7d51b16ae6982e5b82e42bfa1dba043f84
bhlokana <bhlokana@microsoft.com>
2021-02-19 14:10:02 -0600

    add defaults

--- InfrastructureAsCode/Modules/Terraform/AppService/variables.tf
+++ InfrastructureAsCode/Modules/Terraform/AppService/variables.tf
@@ -16,6 +16,7 @@ variable "resource_group_name" {
 variable "tags" {
   type        = map
   description = "A key-value pair of tags."
+  default     = null
 }
 
 variable "app_service_plan_id" {
@@ -26,79 +27,95 @@ variable "app_service_plan_id" {
 variable "client_affinity_enabled" {
   type        = bool
   description = "Should the App Service send session affinity cookies, which route client requests in the same session to the same instance?"
+  default     = false
 }
 
 variable "client_cert_enabled" {
   type        = bool
   description = "A key-value pair of App Settings."
+  default     = false
 }
 
 variable "enabled" {
   type        = bool
   description = "Is the App Service Enabled?"
+  default     = true
 }
 
 variable "https_only" {
   type        = bool
   description = "Can the App Service only be accessed via HTTPS? Defaults to false."
+  default     = false
 }
 
 variable "logs_map" {
   type        = map
   description = "Map of log data"
+  default     = null 
 }
 
 variable "site_config_map" {
   type        = map
   description = "Map of site config data"
+  default     = null
 }
 
 variable "site_config_cors_map" {
   type        = map
   description = "Map of cors data"
+  default     = null
 }
 
 variable "identity_map" {
   type        = map
   description = "Map of identity data"
+  default     = null
 }
 
 variable "app_settings_map" {
   type        = map
   description = "Map of app settings"
+  default     = null
 }
 
 variable "auth_settings_map" {
   type        = map
   description = "Map of authentication settings data"
+  default     = null
 }
 
 variable "active_directory_map" {
   type        = map
   description = "Map of active directory data"
+  default     = null
 }
 
 variable "storage_account_map" {
   type        = map
   description = "Map of storage account data"
+  default     = null
 }
 
 variable "backup_map" {
   type        = map
   description = "Map of backup data"
+  default     = null
 }
 
 variable "schedule_map" {
   type        = map
   description = "Map of backup schedule data"
+  default     = null
 }
 
 variable "connection_string_map" {
   type        = map
   description = "Map of connection string data"
+  default     = null
 }
 
 variable "ip_restriction_map" {
   type        = map
   description = "Map of ip restriction data"
+  default     = null
 }

7728b21415f2c7abe4e32134357626367af939d9
bhlokana <bhlokana@microsoft.com>
2021-02-19 17:52:26 -0600

    remove app settings map

--- InfrastructureAsCode/Modules/Terraform/AppService/main.tf
+++ InfrastructureAsCode/Modules/Terraform/AppService/main.tf
@@ -3,7 +3,6 @@ resource "azurerm_app_service" "app_service" {
   location                = var.location
   resource_group_name     = var.resource_group_name
   app_service_plan_id     = var.app_service_plan_id
-  app_settings            = local.app_settings_map
   client_affinity_enabled = var.client_affinity_enabled
   client_cert_enabled     = var.client_cert_enabled
   enabled                 = var.enabled

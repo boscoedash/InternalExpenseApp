f7a89a8539eca719ca33a30ff9a7ffe66377d460
bhlokana <bhlokana@microsoft.com>
2021-03-03 16:03:23 -0600

    add internal app

--- Source/Relecloud.FunctionApp/EventProcessor/Event.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Relecloud.FunctionApp.EventProcessor
-{
-    public class Event
-    {
-        public string EventType { get; set; }
-        public string EntityId { get; set; }
-    }
-}

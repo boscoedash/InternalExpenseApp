e6ec6425831d4e9631fd022629b0d111545512d1
bhlokana <bhlokana@microsoft.com>
2021-03-09 14:53:56 -0600

    test selenium install

---
+++ .github/workflows/CI/Selenium/Untitled3.ps1
@@ -0,0 +1,10 @@
+﻿$path = $PSScriptRoot
+
+while ((Split-Path -Path $path -Leaf) -ne '.github')
+{
+    $path = Split-Path -Path $path -Parent
+}
+
+$root = Split-Path -Path $path -Parent
+
+write-host $path
